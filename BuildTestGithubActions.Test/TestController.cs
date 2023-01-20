8a8748aaa705acc5f871bbc8730f9e0efe42f3c0
Cristian Torres <ctorres@codisa.com>
2023-01-20 17:24:41 -0600

    Add project files.

---
+++ BuildTestGithubActions.Test/TestController.cs
@@ -0,0 +1,29 @@
+using BuildTestGithubActions.Api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System.Net;
+
+namespace BuildTestGithubActions.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            //Arrange 
+            string text = "Hello Word";
+
+            //Act
+            TestController controller = new TestController();
+            ObjectResult responseController = (ObjectResult)controller.Get(text);
+
+            //Assert
+            Assert.AreEqual((int)HttpStatusCode.OK, responseController.StatusCode!.Value);
+        }
+    }
+}
